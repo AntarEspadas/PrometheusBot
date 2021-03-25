65079f509dc360a538ae41ec37690c75904fba50
Naratna <antar.espadas@hotmail.com>
2021-03-25 05:01:11 -0600

    Added 'Avatar' command

---
+++ PrometheusBot/Modules/Utility/AvatarModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace PrometheusBot.Modules.Utility
+{
+    public class AvatarModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("Avatar")]
+        [Alias("Profile", "pfp")]
+        public async Task Avatar([Remainder] IUser user)
+        {
+            string pfp = user.GetAvatarUrl(size:256);
+            await ReplyAsync(pfp);
+        }
+    }
+}
