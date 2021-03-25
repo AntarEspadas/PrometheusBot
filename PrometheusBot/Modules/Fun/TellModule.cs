3914f5dd320855f96b440e84c84c61971d1c75cb
Naratna <antar.espadas@hotmail.com>
2021-03-25 06:17:30 -0600

    Added 'Tell' command

---
+++ PrometheusBot/Modules/Fun/TellModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace PrometheusBot.Modules.Fun
+{
+    public class TellModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("Tell")]
+        public async Task Tell(SocketUser user, [Remainder] string sentence)
+        {
+            Regex regex = new("to go fuck (himself)|(herself)|(themselves)|(itself)");
+            if (regex.IsMatch(sentence))
+            {
+                var application = await Context.Client.GetApplicationInfoAsync().ConfigureAwait(false);
+                if (Context.User.Id == application.Owner.Id)
+                {
+                    await ReplyAsync($"I love you {user.Mention}");
+                }
+                else
+                {
+                    await ReplyAsync($"{user.Mention}, go fuck yourself");
+                }
+            }
+        }
+    }
+}
