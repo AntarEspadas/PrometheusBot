ddf3af4b21f82a0c0934f51bee1bd9df9c6d3101
Naratna <antar.espadas@hotmail.com>
2021-03-25 04:50:33 -0600

    Added 'A' command

---
+++ PrometheusBot/Modules/Fun/AModule.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace PrometheusBot.Modules.Fun
+{
+    public class AModule : ModuleBase<SocketCommandContext>
+    {
+        private static readonly string _aGif = Path.Combine(Program.Directory, "assets", "a", "a.gif");
+
+        [ACommand]
+        public async Task A()
+        {
+            if (!File.Exists(_aGif)) return;
+            await Context.Channel.SendFileAsync(_aGif);
+        }
+    }
+
+    class ACommandAttribute : NonStandardCommandAttribute
+    {
+        static readonly HashSet<string> _a;
+        static ACommandAttribute()
+        {
+            string aPath = Path.Combine(Program.Directory, "assets", "a", "a.txt");
+            try
+            {
+                _a = new HashSet<string>(File.ReadAllLines(aPath));
+            }
+            catch
+            {
+                _a = new HashSet<string>(new[] { "a", "A" });
+            }
+        }
+        public override bool Validate(ICommandContext context)
+        {
+            string strippedContent = StripNonSpacingMarks(context.Message.Content);
+            strippedContent = strippedContent.Trim();
+            strippedContent = strippedContent.Trim(new[] { '\u200B', '\uFEFF' });
+            return _a.Contains(strippedContent);
+        }
+        private static string StripNonSpacingMarks(string str)
+        {
+            var stringBuilder = new StringBuilder(str.Length);
+            foreach (char chr in str)
+            {
+                var category = CharUnicodeInfo.GetUnicodeCategory(chr);
+                if (category != UnicodeCategory.NonSpacingMark)
+                    stringBuilder.Append(chr);
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
