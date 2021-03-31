5ae1edf3bb441cd74434ab1fb9f72916447d3372
Naratna <antar.espadas@hotmail.com>
2021-03-30 18:05:05 -0600

    Added 'Ping' command

---
+++ PrometheusBot/Modules/Utility/PingModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace PrometheusBot.Modules.Utility
+{
+    public class PingModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("Ping")]
+        public async Task Ping()
+        {
+            EmbedBuilder embedBuilder = new()
+            {
+                Title = "Latency",
+                Description = "Pinging..."
+            };
+
+            var startTime = DateTime.Now;
+            var message = await ReplyAsync(embed: embedBuilder.Build());
+            var endTime = DateTime.Now;
+            int msgChannelLatency = endTime.Subtract(startTime).Milliseconds;
+            int apiLatency = Context.Client.Latency;
+
+            string msg = $"**Message channel Latency**: {msgChannelLatency}ms.\n**Discord API latency**: {apiLatency}ms";
+            embedBuilder.Description = msg;
+            await message.ModifyAsync(message => message.Embed = embedBuilder.Build());
+        }
+    }
+}
